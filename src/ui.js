ba79c996307a46294b747d23be510887c9a6e9f9
mantesso <fmantesso@gmail.com>
2024-01-25 15:28:52 +0100

    current project selection and ui

--- src/ui.js
+++ src/ui.js
@@ -6,6 +6,8 @@ let projectForm = document.getElementById("projectForm");
 const projectList = document.getElementById("projectList");
 const projectTemplate = document.getElementById("projectTemplate");
 
+let currentProjectId;
+
 projectForm.addEventListener("submit", (event) => {
   event.preventDefault();
   let formContent = event.target.elements;
@@ -29,6 +31,7 @@ function displayProjects() {
 
     const clone = document.importNode(projectTemplate.content, true);
     clone.querySelector("li").id = projectId;
+    clone.querySelector("li").addEventListener("click", selectCurrentProject);
     clone.querySelector("p").textContent = projectName;
     clone
       .querySelector("button")
@@ -41,6 +44,21 @@ function removeProjectFromList(e) {
   const projectId = e.target.parentNode.parentNode.id;
   deleteProject(projectId);
   displayProjects();
+}
+
+function selectCurrentProject(e) {
+  const selectedProject = e.target.closest("li");
+  currentProjectId = selectedProject.id;
+  console.log(`current project = ${currentProjectId}`);
+
+  let projects = projectList.children;
+  for (let i = 0, n = projects.length; i < n; i++) {
+    projects[i].classList.remove("bg-blue-100");
+    projects[i].classList.add("bg-gray-100");
+  }
+
+  selectedProject.classList.remove("bg-gray-100");
+  selectedProject.classList.add("bg-blue-100");
 }
 
 export { displayProjects };
